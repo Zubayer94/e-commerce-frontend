6225d2612065a82b2d8bf54948899ba683930e9c
zubayer94 <zubayer94@gmail.com>
2022-03-10 17:43:51 +0600

    fixed navbar and added logo

--- components/home/AppNavBar.vue
+++ components/home/AppNavBar.vue
@@ -7,15 +7,26 @@
       </button>
 
       <div class="collapse navbar-collapse" id="main_nav">
-        <ul class="navbar-nav mx-auto ">
+        <ul class="navbar-nav mx-auto list-inline">
           <li class="nav-item">
-            <a class="nav-link" href="#">Buy</a>
+            <a class="nav-link" href="#">
+              <BuyLogo :height="25" :width="25" />
+              &nbsp; <span class="h5" style="color: #3858a5" >Buy</span>
+            </a>
           </li>
+
           <li class="nav-item">
-            <a class="nav-link" href="#">Sell</a>
+            <a class="nav-link" href="#">
+              <SwapLogo :height="20" :width="20" :color="red" />
+              &nbsp; <span class="h5" style="background: -webkit-linear-gradient(right, #E87C28, #3858a5); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">Swap</span>
+            </a>
           </li>
+
           <li class="nav-item">
-            <a class="nav-link" href="#">Swap</a>
+            <a class="nav-link" href="#">
+              <SellLogo :height="25" :width="25"/>
+              &nbsp; <span class="h5" style="color: #E87C28">Sell</span>
+            </a>
           </li>
           <!-- <li class="nav-item">
             <a class="nav-link" href="#">Partnership</a>
@@ -50,7 +61,11 @@
 </template>
 
 <script>
+import SellLogo from '~/components/custom/SellLogo.vue'
+import SwapLogo from '~/components/custom/SwapLogo.vue'
+import BuyLogo from '~/components/custom/BuyLogo.vue'
 export default {
+  components: { SwapLogo, SellLogo, BuyLogo },
 
 }
 </script>
