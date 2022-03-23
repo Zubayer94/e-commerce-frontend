781aa772f4d8c0de8c7f8e28a55392886f186315
zubayer94 <zubayer94@gmail.com>
2022-03-23 17:01:25 +0600

    added buy scraffold

---
+++ layouts/buy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <AppHeader />
+
+    <nuxt/>
+
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppHeader from '../components/home/AppHeader.vue'
+import AppFooter from '../components/home/AppFooter.vue'
+export default {
+  components: { AppHeader, AppFooter },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/buy/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Content />
+</template>
+
+<script>
+import Content from '../../components/home/Content.vue'
+export default {
+  components: { Content },
+  layout: 'buy',
+
+}
+</script>
+
+<style>
+
+</style>
